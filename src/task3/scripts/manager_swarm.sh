7f07c3f9afa8abd8b5f731373539e52072801f7b
krsvaaa <211358270+krsvaaa@users.noreply.github.com>
2026-02-13 16:04:58 +0300

    Updated readme

---
+++ src/task3/scripts/manager_swarm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+TOKEN=$(docker swarm init --advertise-addr 192.168.56.10 | awk 'NR==5')
+
+rm /vagrant/scripts/token.sh
+
+echo "#!/bin/bash" >> /vagrant/scripts/token.sh
+echo "$TOKEN" >> /vagrant/scripts/token.sh
+
+chmod + x /vagrant/scripts/token.sh
+
+
---
+++ src/task3/scripts/token.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+    docker swarm join --token SWMTKN-1-13b8cv7sz438aygfjz4lz2pbfos5dk8v2hnsjflz28w9ducewj-7qywjbau0k58lofgigvwg8c02 192.168.56.10:2377
